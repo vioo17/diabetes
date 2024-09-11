202fb0a30747b9b1a8f993da13b78e1b9a3df990
vioo17 <117342888+vioo17@users.noreply.github.com>
2024-09-11 15:32:30 +0700

    Latihan_Tugas1.py

--- Latihan_Tugas1.py
+++ Latihan_Tugas1.py
@@ -3,9 +3,9 @@ import pandas as pd
 import pickle
 import os
 #load model
-model_directory = r'D:\models' ##diisi dengan path folder dimana file model berada
+
 # Gunakan os.path.join() untuk menggabungkan direktori dan file model pickle
-model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl')
+model_path = 'rf_diabetes_model.pkl'
 # Periksa apakah file ada di direktori yang ditentukan
 if os.path.exists(model_path):
     try:
